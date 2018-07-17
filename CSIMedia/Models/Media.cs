f0246176a3e1ef7b92ae47257ea2801f2acda7db
ItzikFekete <34029678+ItzikFekete@users.noreply.github.com>
2018-07-18 00:27:42 +0100

    Add project files.

---
+++ CSIMedia/Models/Media.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSIMedia.Models
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+    public class MediaDBContext : DbContext
+    {
+        public DbSet<Media> Medias { get; set; }
+
+        public System.Data.Entity.DbSet<CSIMedia.Table> Tables { get; set; }
+    }
+}
