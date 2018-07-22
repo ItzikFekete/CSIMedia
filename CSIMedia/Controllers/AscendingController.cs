5012aaa31388256cea6d04029fad5c791cd16dbf
ItzikFekete <34029678+ItzikFekete@users.noreply.github.com>
2018-07-22 16:16:21 +0100

    added JQuery

---
+++ CSIMedia/Controllers/AscendingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSIMedia.Controllers
+{
+    public class AscendingController : Controller
+    {
+        // GET: Ascending
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Ascending/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Ascending/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ascending/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ascending/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Ascending/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ascending/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Ascending/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- CSIMedia/Scripts/ListOrganiser.js
+++ CSIMedia/Scripts/ListOrganiser.js
@@ -1,2 +1,15 @@
-﻿var listOrder = $('.listOrder').get().map(function (el) { return el.value });
+﻿console.log('test');
+//var listOrder = $('.listOrder').get().map(function (el) { return el.value });
+
+/*$('.ascending').click(function () {
+    document.querySelector('.ascendingDisplay').innerHTML(listOrder.sort);
+});
+$('.descending').click(function () {
+    document.querySelector('.descendingDisplay').innerHTML(listOrder.reverse);
+});*/ 
+
+$(function () {
+    var $clone = $('.indexA, indexAInput').clone();
+    $('.clonedA').html($clone).text; 
+});
 
